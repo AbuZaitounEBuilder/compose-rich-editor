70796d8632a2cf2472d17beb7508f8fecbb38a16
Anas <anas_abuzaitoun@trimble.com>
2026-05-16 18:05:13 -0400

    Changed version name

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -1,4 +1,4 @@
 allprojects {
-    group = "com.mohamedrejeb.richeditor"
-    version = System.getenv("VERSION") ?: "1.0.0-rc14"
+    group = "io.github.abuzaitounebuilder"
+    version = System.getenv("VERSION") ?: "1.0.0-rc15"
 }
